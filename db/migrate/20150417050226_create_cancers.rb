7aa0083143e6565d6230164f83942bda4169c5b4
Shlomo Twerski <ystwerski@gmail.com>
2015-04-17 02:11:59 -0500

    Thought about it. No way for a two way has_many. Must be Patient has_many cancers but cancer belongs_to patient

--- db/migrate/20150417050226_create_cancers.rb
+++ db/migrate/20150417050226_create_cancers.rb
@@ -1,6 +1,7 @@
 class CreateCancers < ActiveRecord::Migration
   def change
     create_table :cancers do |t|
+      t.integer :patient_id
       t.string :cancer_type
       t.integer :age_of_diagnosis
 
